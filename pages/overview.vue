b771e3db5d31a75fcc1e8d332d1ed40d905a995c
김준태 <bexellenting@gmail.com>
2021-05-07 16:42:45 +0900

    Create a dashboard overview page

---
+++ pages/overview.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col cols>
+        overview 페이지
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  layout: 'dashboard'
+}
+</script>
+
+<style>
+
+</style>
