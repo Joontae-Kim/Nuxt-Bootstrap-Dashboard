cb93e5c505d31b715561e6a05ed7bab432ad87ba
김준태 <bexellenting@gmail.com>
2021-05-07 16:42:13 +0900

    Create a dashboard layout and component

---
+++ components/dashboard/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">
+        Home
+      </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <div class="sidebar">sidebar</div>
+    <div class="content-page">
+      <!-- <div>네비게이션바</div> -->
+      <DashboardHeader />
+      <div class="content p-2">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
