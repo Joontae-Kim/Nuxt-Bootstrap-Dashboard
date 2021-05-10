d84c43be6e27d3e6711eacc5d6c254c8f0b711d9
김준태 <bexellenting@gmail.com>
2021-05-10 16:11:01 +0900

    Edit syntax

--- api/test.js
+++ api/test.js
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  console.log('/api/test');
-  res.send('/api/test');
+  console.log('/api/test')
+  res.send('/api/test')
 })
 
 module.exports = app
