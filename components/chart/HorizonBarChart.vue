3a428f9f863a3f6d834b35ad5ffad9d799d74e8f
김준태 <bexellenting@gmail.com>
2021-05-22 16:01:24 +0900

    Create components of chart

---
+++ components/chart/HorizonBarChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="{'position: relative; height:40vh; width:80vw': option.responsive}">
+    <canvas :id="canvasId" v-bind="{width, height}" />
+  </div>
+</template>
+<script>
+import defaultProps from "~/mixins/chart/defaultProps"
+
+export default {
+  mixins: [defaultProps],
+  data: () => ({
+    type: 'horizontalBar',
+    option: {
+      scales: {
+        yAxes: [
+          { ticks: { beginAtZero: true } }
+        ],
+        xAxes: [
+          { ticks: { beginAtZero: true } }
+        ]
+      }
+    }
+  }),
+  mounted () {},
+  methods: {}
+}
+</script>
---
+++ components/chart/PolarArea.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :style="{'position: relative; height:40vh; width:80vw': option.responsive}">
+    <canvas :id="canvasId" v-bind="{width, height}" />
+  </div>
+</template>
+<script>
+import defaultProps from "../../mixins/chart/defaultProps"
+
+export default {
+  mixins: [defaultProps],
+  data: () => ({
+    type: 'polarArea'
+  }),
+  mounted () {},
+  methods: {}
+}
+</script>
