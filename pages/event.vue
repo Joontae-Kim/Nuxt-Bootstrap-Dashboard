16010130cef103f4c1e5b396971c79edc9bccafb
김준태 <bexellenting@gmail.com>
2021-05-22 16:38:09 +0900

    Create a page of event

---
+++ pages/event.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container fluid>
+    event
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Event',
+  layout: 'dashboard'
+}
+</script>
+
+<style>
+
+</style>
