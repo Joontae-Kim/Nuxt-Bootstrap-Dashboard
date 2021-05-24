0114e2498ac9023a13426bea391114b1be96e74b
김준태 <bexellenting@gmail.com>
2021-05-24 18:21:37 +0900

    fetch /api/event

--- pages/event.vue
+++ pages/event.vue
@@ -1,13 +1,36 @@
 <template>
   <b-container fluid>
-    event
+    <b-row>
+      <b-col cols>
+        {{ res }}
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 export default {
   name: 'Event',
-  layout: 'dashboard'
+  layout: 'dashboard',
+  data: () => ({
+    res: {}
+  }),
+  async fetch () {
+    this.res = await this.$axios.$get('/api/event')
+  },
+  watch: {
+    '$fetchState.pending': {
+      immediate: true,
+      handler (state) {
+        if (typeof state !== 'undefined') {
+          this.$nuxt.$emit('pageLoading', state)
+        }
+      }
+    }
+  },
+  created () {},
+  mounted () {},
+  methods: {}
 }
 </script>
 
