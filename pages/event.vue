cd02cc90630f54b6fdc32754c07d970ed14d3fb1
김준태 <bexellenting@gmail.com>
2021-05-29 04:44:12 +0900

    Develop the event page

--- pages/event.vue
+++ pages/event.vue
@@ -1,5 +1,98 @@
 <template>
   <b-container fluid>
+    <b-row class="mb-2">
+      <b-col cols>
+        <dash-card :use-title="false" class="p-3">
+          <b-row align-v="center">
+            <b-col cols>
+              <b-btn title="Add new Event" variant="primary">
+                <b-icon icon="plus-circle-fill" scale="0.85" aria-hidden="true" shift-v="0" class="mr-2" />
+                <span>Add Event</span>
+              </b-btn>
+            </b-col>
+            <b-col cols>
+              <div class="d-flex align-items-center justify-content-end">
+                <b-btn class="mr-2" variant="light">
+                  <b-icon
+                    icon="arrow-clockwise"
+                    font-scale="0.95"
+                    aria-hidden="true"
+                    aria-label="Refresh"
+                    shift-v="2"
+                  />
+                </b-btn>
+                <b-btn variant="danger">
+                  <b-icon icon="trash" font-scale="0.9" aria-hidden="true" aria-label="Delete selected event" shift-v="2" />
+                </b-btn>
+              </div>
+            </b-col>
+          </b-row>
+        </dash-card>
+      </b-col>
+    </b-row>
+    <b-row class="mb-4">
+      <b-col cols>
+        <dash-card :use-title="false" class="p-3">
+          <b-row align-v="center">
+            <b-col cols>
+              <div class="d-flex align-items-center">
+                <span class="mr-2">Display</span>
+                <b-form-select v-model="selected" :options="options" class="mr-1 w-auto" size="sm" />
+                <span>events</span>
+              </div>
+            </b-col>
+            <b-col cols>
+              <div class="d-flex align-items-center justify-content-end">
+                <b-form-input aria-label="Search" class="w-50 mr-2" />
+                <b-btn variant="light">
+                  <b-icon
+                    icon="filter"
+                    font-scale="0.95"
+                    aria-hidden="true"
+                    aria-label="Refresh"
+                    shift-v="2"
+                  />
+                </b-btn>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols class="overflow-auto">
+              <b-table
+                :sort-by.sync="eventTableOpt.SortBy"
+                :sort-desc.sync="eventTableOpt.SortDesc"
+                :items="events"
+                :fields="eventField"
+                head-variant="light"
+                responsive
+                hover
+              >
+                <template #cell(index)="data">
+                  {{ data.index + 1 }}
+                </template>
+
+                <template #cell(sales)="data">
+                  $ {{ data.value }}
+                </template>
+
+                <template #cell(bounce)="data">
+                  {{ data.value }} %
+                </template>
+
+                <template #cell(actions)>
+                  <b-btn variant="link" class="text-decoration-none p-0 mr-2 shadow-none">
+                    <b-icon icon="trash" scale="1.0" class="icon-danger" />
+                  </b-btn>
+                  <b-btn variant="link" class="text-decoration-none p-0 shadow-none">
+                    <b-icon icon="pencil-square" scale="1.0" class="icon-secondary" />
+                  </b-btn>
+                </template>
+              </b-table>
+            </b-col>
+          </b-row>
+        </dash-card>
+      </b-col>
+    </b-row>
     <b-row>
       <b-col cols>
         {{ events }}
@@ -14,10 +107,73 @@ import { mapGetters } from "vuex"
 export default {
   name: 'Event',
   layout: 'dashboard',
-  data: () => ({}),
+  data: () => ({
+    selected: 15,
+    options: [
+      { text: 15, value: 15 },
+      { text: 30, value: 30 },
+      { text: 45, value: 45 },
+      { text: 100, value: 100 }
+    ],
+    eventTableOpt: {
+      SortBy: 'sales',
+      SortDesc: true
+    },
+    eventField: [
+      'index',
+      {
+        label: 'Title',
+        key: 'title',
+        sortable: true,
+        thClass: '',
+        tdClass: 'w-100 text-gray-600'
+      },
+      {
+        label: 'Open',
+        key: 'openAt',
+        sortable: true,
+        thClass: 'text-nowrap',
+        tdClass: 'text-nowrap w-50 text-gray-600'
+      },
+      {
+        label: 'Close',
+        key: 'closedAt',
+        sortable: true,
+        thClass: 'text-nowrap',
+        tdClass: 'text-nowrap w-50 text-gray-600'
+      },
+      {
+        label: 'Views',
+        key: 'views',
+        sortable: true,
+        thClass: 'text-nowrap',
+        tdClass: 'text-nowrap w-50 text-gray-600'
+      },
+      {
+        label: 'Bounce',
+        key: 'bounce',
+        sortable: true,
+        thClass: 'text-nowrap',
+        tdClass: 'text-nowrap w-50 text-gray-600'
+      },
+      {
+        label: 'Sales',
+        key: 'sales',
+        sortable: true,
+        thClass: 'text-nowrap',
+        tdClass: 'text-nowrap w-50 text-gray-600'
+      },
+      {
+        label: 'Actions',
+        key: 'actions',
+        sortable: false,
+        tdClass: 'align-middle text-center'
+      }
+    ]
+  }),
   async fetch () {
     const res = await this.$axios.$get('/api/event')
-    this.$store.commit('events/add', res)
+    await this.$store.dispatch('events/DISPATCH_ADD', res.list)
   },
   computed: {
     ...mapGetters({
