990bbfaacc342105818dcf856601c58927e8d6e8
김준태 <bexellenting@gmail.com>
2021-05-31 16:43:38 +0900

    Enhanced a feautre of event store

--- store/events.js
+++ store/events.js
@@ -11,6 +11,9 @@ export const getters = {
 }
 
 export const mutations = {
+  set (state, newList) {
+    state.list = newList
+  },
   add (state, newList) {
     if (Array.isArray(newList)) {
       state.list = state.list.concat(newList)
@@ -21,6 +24,10 @@ export const mutations = {
 }
 
 export const actions = {
+  async DISPATCH_SET ({ commit }, events) {
+    const transformed = await transformMultiEvent(events)
+    commit('set', transformed)
+  },
   async DISPATCH_ADD ({ commit }, events) {
     const transformed = await transformMultiEvent(events)
     commit('add', transformed)
