b2af133f04d8521499e8ac1f15863706475504be
김준태 <bexellenting@gmail.com>
2021-06-11 01:15:41 +0900

    Create a component of DoughnutChart

---
+++ components/chart/DoughnutChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <canvas :id="canvasId" v-bind="{width, height}" />
+</template>
+<script>
+import defaultProps from "../../mixins/chart/defaultProps"
+
+export default {
+  mixins: [defaultProps],
+  data: () => ({
+    type: 'doughnut'
+  }),
+  mounted () {},
+  methods: {}
+}
+</script>
