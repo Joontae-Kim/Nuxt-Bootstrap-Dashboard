e91744769a74f08a5766bd438fc3be59979fdd61
김준태 <bexellenting@gmail.com>
2021-06-22 04:20:46 +0900

    Create a feature of querying users api

--- api/lib/users.js
+++ api/lib/users.js
@@ -1,6 +1,8 @@
 const dayjs = require('dayjs')
+const FuzzySearch = require('fuzzy-search')
 const { randomIndex, createSerialRandom } = require('../utility/createRandom')
 const { createDateArray } = require('../utility/dates')
+const findArray = require('../utility/findArray')
 
 export function createUsersTraffic (startAt, count = 7) {
   return new Promise((resolve, reject) => {
@@ -38,6 +40,97 @@ export function createUsersTraffic (startAt, count = 7) {
         timeSeriesTraffic
       })
     } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+export function searchUser (userSet, query) {
+  return new Promise((resolve, reject) => {
+    try {
+      let searched = userSet
+
+      if (searched.length && query.id) {
+        searched = findArray.Search(searched, 'eq', 'id', query.id)
+      }
+
+      if (searched.length && query.uid) {
+        const uidSearcher = new FuzzySearch(searched, ['uid'], {
+          caseSensitive: false
+        })
+        searched = uidSearcher.search(query.uid)
+      }
+
+      if (query.username) {
+        const usernameSearcher = new FuzzySearch(searched, ['username'], {
+          caseSensitive: false
+        })
+        searched = usernameSearcher.search(query.username)
+      }
+
+      if (query.email) {
+        const emailSearcher = new FuzzySearch(searched, ['email'], {
+          caseSensitive: false
+        })
+        searched = emailSearcher.search(query.email)
+      }
+
+      if (query.address) {
+        const addressSearcher = new FuzzySearch(searched, ['address.full'], {
+          caseSensitive: false
+        })
+        searched = addressSearcher.search(query.address)
+      }
+
+      if (searched.length && query.phoneNumber) {
+        searched = findArray.Search(searched, 'eq', 'phone_number', query.phoneNumber)
+      }
+
+      if (query.creditCard) {
+        const creditCardSearcher = new FuzzySearch(searched, ['credit_card.cc_number'], {
+          caseSensitive: true
+        })
+        searched = creditCardSearcher.search(query.creditCard)
+      }
+
+      if (searched.length && query.payment) {
+        if (query.payment.includes('null')) {
+          const nullIndex = query.payment.findIndex(ele => ele === 'null')
+          query.payment[nullIndex] = null
+        }
+        searched = searched.reduce((paymentSearched, user) => {
+          const isIncludes = findArray.partialInclude(query.payment, [user.payment])
+          if (isIncludes) {
+            paymentSearched.push(user)
+          }
+          return paymentSearched
+        }, [])
+      }
+
+      if (searched.length && query.authentication) {
+        searched = searched.reduce((authSearched, user) => {
+          const isIncludes = findArray.partialInclude(query.authentication, user.authentication)
+          if (isIncludes) {
+            authSearched.push(user)
+          }
+          return authSearched
+        }, [])
+      }
+
+      if (searched.length && query.status) {
+        searched = searched.reduce((statusSearched, user) => {
+          const isIncludes = findArray.partialInclude(query.status, user.status)
+          if (isIncludes) {
+            statusSearched.push(user)
+          }
+          return statusSearched
+        }, [])
+      }
+
+      resolve(searched)
+    } catch (err) {
+      console.log(err)
+      console.log(err.message)
       reject(err)
     }
   })
