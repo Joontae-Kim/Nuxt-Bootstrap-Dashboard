f3baa12d26a9ddde400be267e60fad05892a4a6e
김준태 <bexellenting@gmail.com>
2021-06-29 17:08:16 +0900

    Enhanced a feature of rendering chart

--- components/chart/BarChart.vue
+++ components/chart/BarChart.vue
@@ -1,8 +1,9 @@
 <template>
   <canvas :id="canvasId" v-bind="{width, height}" />
 </template>
+
 <script>
-import defaultProps from "../../mixins/chart/defaultProps"
+import defaultProps from "~/mixins/chart/defaultProps_linebar"
 
 export default {
   mixins: [defaultProps],
@@ -11,22 +12,80 @@ export default {
     option: {
       scales: {
         xAxes: [{
-          gridLines: false
+          gridLines: false,
+          ticks: {
+            padding: 7
+          }
         }],
         yAxes: [{
           gridLines: {
             borderDash: [3, 4]
           },
           ticks: {
-            beginAtZero: true,
-            stepSize: 5,
-            padding: 10
+            beginAtZero: false,
+            padding: 7
           }
         }]
       }
     }
   }),
-  mounted () {},
-  methods: {}
+  methods: {
+    mergeOption () {
+      // merge Default Options ~ defaultProps.js
+      this.mergeDefaultOptions()
+
+      if (this.scalesX) {
+        if (this.scalesX.length === 1) {
+          if (this.scalesX[0].type && this.scalesX[0].type === 'time') {
+            this.option.scales.xAxes[0] = this.scalesX[0]
+            if (!this.scalesX[0].time) {
+              this.option.scales.xAxes[0].time = { unit: 'hour', distribution: 'series', displayFormats: { minute: 'h:mm a' }, stepSize: 4 }
+            }
+          } else {
+            this.option.scales.xAxes[0] = this.mergeOptions(this.option.scales.xAxes[0], this.scalesX[0])
+          }
+        } else {
+          this.option.scales.xAxes = this.mergeOptions(this.option.scales.xAxes, this.scalesX)
+        }
+      }
+
+      if (this.scalesY) {
+        if (this.scalesY.length === 1) {
+          if (this.scalesY[0].type && this.scalesY[0].type === 'time') {
+            this.option.scales.yAxes[0] = this.scalesY[0]
+          } else {
+            this.option.scales.yAxes[0] = this.mergeOptions(this.option.scales.yAxes[0], this.scalesY[0])
+          }
+        } else {
+          this.option.scales.yAxes = this.mergeOptions(this.option.scales.yAxes, this.scalesY)
+        }
+      }
+
+      let options = {
+        legend: {
+          display: this.legendView
+        },
+        ...this.option
+      }
+
+      if (this.customOpt) {
+        options = this.mergeOptions(options, this.customOpt)
+      }
+      return options
+    },
+    renderChart () {
+      try {
+        const ctx = document.getElementById(this.canvasId).getContext('2d')
+        const options = this.mergeOption()
+        this.$chartjs.createChart(ctx, {
+          type: this.type,
+          data: this.data,
+          options
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 }
 </script>
