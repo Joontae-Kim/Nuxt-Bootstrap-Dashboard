7cd4e0e2b475d7695db18e931d9a23d58bdcb35f
김준태 <bexellenting@gmail.com>
2021-06-30 12:52:24 +0900

    Change dash-header component to dash-nav

---
+++ components/dashboard/nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-navbar type="dark" variant="white" class="bg-transparent">
+    <b-navbar-nav align="center">
+      <b-nav-text>
+        <div class="cursor-pointer" @click="toggleSidebar">
+          <b-icon-list color="#212529" font-scale="1.5" aria-hidden="true" variant="light" />
+        </div>
+      </b-nav-text>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'DashboardNav',
+  methods: {
+    toggleSidebar () {
+      this.$emit('toggleSidebar')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
