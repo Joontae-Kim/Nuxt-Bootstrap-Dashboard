0332eee25bca0dde70e16c804dc08c769a5b37ac
김준태 <bexellenting@gmail.com>
2021-07-05 17:55:57 +0900

    Create a chart color set

---
+++ mixins/chart/colorSet.js
@@ -0,0 +1,39 @@
+export const colorTypes = [
+  "Blue",
+  "LightBlue",
+  "Indigo",
+  "BlueGray",
+  "placeholder"
+]
+
+export const colorSet = [
+  { type: "Blue", rgb: "rgb(0, 60, 143)", border: "rgb(255, 255, 255)", background: "rgba(0, 60, 143, .5)" },
+  { type: "Blue", rgb: "rgb(21, 101, 192)", border: "rgb(233, 236, 239)", background: "rgba(21, 101, 192, .5)" },
+  { type: "Blue", rgb: "rgb(30, 136, 229)", border: "rgb(224, 224, 224)", background: "rgba(30, 136, 229, .5)" },
+  { type: "Blue", rgb: "rgb(100, 181, 246)", border: "rgb(189, 189, 189)", background: "rgba(100, 181, 246, .5)" },
+  { type: "LightBlue", rgb: "rgb(3, 155, 229)", border: "rgb(255, 255, 255)", background: "rgba(3, 155, 229, .5)" },
+  { type: "LightBlue", rgb: "rgb(3, 169, 244)", border: "rgb(233, 236, 239)", background: "rgba(3, 169, 244, .5)" },
+  { type: "LightBlue", rgb: "rgb(41, 182, 246)", border: "rgb(224, 224, 224)", background: "rgba(41, 182, 246, .5)" },
+  { type: "LightBlue", rgb: "rgb(79, 195, 247)", border: "rgb(189, 189, 189)", background: "rgba(79, 195, 247, .5)" },
+  { type: "Indigo", rgb: "rgb(57, 73, 171)", border: "rgb(255, 255, 255)", background: "rgba(57, 73, 171, .5)" },
+  { type: "Indigo", rgb: "rgb(63, 81, 181)", border: "rgb(233, 236, 239)", background: "rgba(63, 81, 181, .5)" },
+  { type: "Indigo", rgb: "rgb(92, 107, 192)", border: "rgb(224, 224, 224)", background: "rgba(92, 107, 192, .5)" },
+  { type: "BlueGray", rgb: "rgb(84, 110, 122)", border: "rgb(255, 255, 255)", background: "rgba(84, 110, 122, .5)" },
+  { type: "Indigo", rgb: "rgb(121, 134, 203)", border: "rgb(189, 189, 189)", background: "rgba(121, 134, 203, .5)" },
+  { type: "BlueGray", rgb: "rgb(96, 125, 139)", border: "rgb(233, 236, 239)", background: "rgba(96, 125, 139, .5)" },
+  { type: "BlueGray", rgb: "rgb(120, 144, 156)", border: "rgb(224, 224, 224)", background: "rgba(120, 144, 156, .5)" },
+  { type: "BlueGray", rgb: "rgb(144, 164, 174)", border: "rgb(189, 189, 189)", background: "rgba(144, 164, 174, .5)" },
+  { type: "Brown", rgb: "rgb(93, 64, 55)", border: "rgb(255, 255, 255)", background: "rgba(93, 64, 55, .5)" },
+  { type: "Brown", rgb: "rgb(141, 110, 99)", border: "rgb(233, 236, 239)", background: "rgba(141, 110, 99, .5)" },
+  { type: "Brown", rgb: "rgb(121, 85, 72)", border: "rgb(224, 224, 224)", background: "rgba(121, 85, 72, .5)" },
+  { type: "Brown", rgb: "rgb(109, 76, 65)", border: "rgb(189, 189, 189)", background: "rgba(109, 76, 65, .5)" },
+  { type: "DeepPurple", rgb: "rgb(81, 45, 168)", background: "rgba(81, 45, 168, .5)", border: "rgb(255, 255, 255)" },
+  { type: "DeepPurple", rgb: "rgb(94, 53, 177)", background: "rgba(94, 53, 177, .5)", border: "rgb(233, 236, 239)" },
+  { type: "DeepPurple", rgb: "rgb(103, 58, 183)", background: "rgba(103, 58, 183, .5)", border: "rgb(224, 224, 224)" },
+  { type: "DeepPurple", rgb: "rgb(126, 87, 194)", background: "rgba(126, 87, 194, .5)", border: "rgb(189, 189, 189)" },
+  { type: 'placeholder', rgb: 'rgba(206, 212, 218, 1)' }
+  // { type: "", rgb: "", background: "rgba(, .5)", border: "rgb(255, 255, 255)" },
+  // { type: "", rgb: "", background: "rgba(, .5)", border: "rgb(233, 236, 239)" },
+  // { type: "", rgb: "", background: "rgba(, .5)", border: "rgb(224, 224, 224)" },
+  // { type: "", rgb: "", background: "rgba(, .5)", border: "rgb(189, 189, 189)" },
+]
