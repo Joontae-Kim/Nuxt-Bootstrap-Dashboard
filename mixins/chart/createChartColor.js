5edceda3e0c31aa3f70c8fd909cb31105a62c161
김준태 <bexellenting@gmail.com>
2021-07-05 17:58:10 +0900

    Enhanced a feature of creating chart color

---
+++ mixins/chart/createChartColor.js
@@ -0,0 +1,68 @@
+// /chart/createChartColor.js
+import { colorTypes, colorSet } from './colorSet'
+
+export default {
+  data: () => ({
+    colorSet,
+    doughnutPieColorSets: [
+      'Blue',
+      'Indigo',
+      'BlueGray'
+    ]
+  }),
+  computed: {
+    totalColorCount () {
+      return this.doughnutPieColorSets.length * 4
+    },
+    placeholderColor () {
+      return this.colorSet.find(color => color.type === 'placeholder')
+    }
+  },
+  created () {},
+  mounted () {},
+  methods: {
+    getRandomIntInclusive (max = this.colorSet.length - 1, min = 0) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+    getRandomColor () {
+      const random = this.getRandomIntInclusive()
+      return this.colorSet[random]
+    },
+    getRandomColors (count = 5) {
+      const paletteIdx = []
+      while (paletteIdx.length < count) {
+        const random = this.getRandomIntInclusive()
+        if (!paletteIdx.includes(random)) {
+          paletteIdx.push(random)
+        }
+      }
+      const palette = paletteIdx.reduce((filled, idx) => {
+        filled.push(this.colorSet[idx])
+        return filled
+      }, [])
+      return palette
+    },
+    getColorsByType (types) {
+      if (!types || !colorTypes.includes(types)) {
+        types = colorTypes[Math.floor(Math.random() * colorTypes.length)]
+      }
+
+      if (typeof types === 'string') {
+        const typeColors = this.colorSet.filter(({ type }) => types === type)
+        return typeColors
+      } else {
+        types = types.map((type) => {
+          if (!colorTypes.includes(type)) {
+            return colorTypes[Math.floor(Math.random() * colorTypes.length)]
+          } else {
+            return type
+          }
+        })
+        const typeColors = this.colorSet.filter(([type]) => colorTypes.includes(type))
+        return typeColors
+      }
+    }
+  }
+}
