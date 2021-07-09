2f9bed04f299112ce8c3278e5ce21baa3c751061
김준태 <bexellenting@gmail.com>
2021-07-09 17:41:09 +0900

    Creat a feature of rendering custom legend of chart

--- plugins/chartjs.js
+++ plugins/chartjs.js
@@ -6,7 +6,22 @@ export default ({ app }, inject) => {
     createChart (dom, data) {
       Chart.plugins.register(datalabels)
       // eslint-disable-next-line no-new
-      new Chart(dom, data)
+      const chartIst = new Chart(dom, data)
+      if (data.options.customLegend && !!data.options.customLegendId) {
+        const customLegendDom = document.getElementById(data.options.customLegendId)
+        if (customLegendDom) {
+          customLegendDom.innerHTML = chartIst.generateLegend()
+        }
+
+        if (data.options.useCustomLegendClick !== false) {
+          if (customLegendDom.childElementCount) {
+            const customLegendClick = data.options.customLegendClick
+            customLegendDom.childNodes.forEach((child) => {
+              child.addEventListener("click", customLegendClick.bind(null, chartIst), false)
+            })
+          }
+        }
+      }
     }
   })
 }
