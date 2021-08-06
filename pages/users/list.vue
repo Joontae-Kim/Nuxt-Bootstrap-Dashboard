773b5a02e32348babdc3eb29ef4106d9e34c2f6d
김준태 <bexellenting@gmail.com>
2021-08-07 04:23:35 +0900

    Create a feature of users/ pages

---
+++ pages/users/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col cols>
+        list
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  layout: 'dashboard'
+}
+</script>
+
+<style>
+
+</style>
