ad2910601c5844fc99b026141b689e0169813b5f
김준태 <bexellenting@gmail.com>
2021-08-07 04:22:59 +0900

    Enhanced a feature of users statics

--- api/controller/users.ctrl.js
+++ api/controller/users.ctrl.js
@@ -1,7 +1,7 @@
 const { randomIndex } = require('../utility/createRandom')
 const delay = require('../utility/delayResponse')
 const { ErrorHandler } = require('../utility/error')
-const { createUsersTraffic, searchUser, createRandomAcitivy, createRandomPurchaseList } = require('../lib/users')
+const { createUsersTraffic, searchUser, createRandomAcitivy, createRandomPurchaseList, commonStatics } = require('../lib/users')
 
 const index = async (req, res, next) => {
   try {
@@ -34,24 +34,18 @@ const statics = async (req, res, next) => {
     await delay()
     const { staticDate, staticCount } = req.query
     const { serialTraffic, timeSeriesTraffic } = await createUsersTraffic(staticDate, staticCount)
-    const total = {
-      value: randomIndex(900, 1300),
-      rate: randomIndex(7, 17, false, 1)
-    }
-    const newUsers = {
-      rate: randomIndex(4, 13, false, 1)
-    }
-    newUsers.value = Math.floor(total.value * (newUsers.rate / 100))
+    const commonStaticsSet = commonStatics()
     const activeUsers = {
       rate: randomIndex(34, 56, false, 1)
     }
+    const total = commonStaticsSet.total
     activeUsers.value = Math.floor(total.value * (activeUsers.rate / 100))
     const groupByUsers = { Active: activeUsers.value }
     groupByUsers.Pending_Approval = Math.floor(total.value * (randomIndex(3, 10, false, 1) / 100))
     groupByUsers.Not_Yet_Activated = Math.floor(total.value * (randomIndex(4, 9, false, 1) / 100))
     const groupByValues = Object.values(groupByUsers).reduce((sum, val) => sum + val, 0)
     groupByUsers.Inactive = total.value - groupByValues
-    res.status(200).send({ total, newUsers, activeUsers, groupByUsers, weekSerial: serialTraffic, timeSerial: timeSeriesTraffic })
+    res.status(200).send({ ...commonStaticsSet, activeUsers, groupByUsers, weekSerial: serialTraffic, timeSerial: timeSeriesTraffic })
   } catch (err) {
     console.log(`        ~ err => `, err)
     console.log(`        ~ err.message => `, err.message)
