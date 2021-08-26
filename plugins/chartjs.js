4624cfc90e78b4e84a454325a4b0bbea1aac4f4c
김준태 <bexellenting@gmail.com>
2021-08-26 18:30:31 +0900

    Register datalabels plugin in Global Scope

--- plugins/chartjs.js
+++ plugins/chartjs.js
@@ -3,12 +3,11 @@ const datalabels = require('chartjs-plugin-datalabels')
 const { drawRoundRectangle } = require('~/lib/chart.lib')
 
 Chart.elements.Rectangle.prototype.draw = drawRoundRectangle
+Chart.plugins.register(datalabels)
 
 export default ({ app }, inject) => {
   inject('chartjs', {
     createChart (dom, data) {
-      Chart.plugins.register(datalabels)
-      // eslint-disable-next-line no-new
       const chartIst = new Chart(dom, data)
       if (data.options.customLegend && !!data.options.customLegendId) {
         const customLegendDom = document.getElementById(data.options.customLegendId)
