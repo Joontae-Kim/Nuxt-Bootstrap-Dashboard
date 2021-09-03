78ea75b0805d2e1277affbf70c59b8c581567c8c
김준태 <bexellenting@gmail.com>
2021-09-04 01:42:55 +0900

    Create the ui of the dashboard navigation toolSet

--- components/dashboard/nav.vue
+++ components/dashboard/nav.vue
@@ -1,21 +1,83 @@
 <template>
-  <b-navbar id="dash-header" type="dark" variant="white" class="bg-transparent">
-    <b-navbar-nav align="center">
-      <b-nav-text>
-        <div id="header-sidebar-toggle" class="cursor-pointer" @click="toggleSidebar">
-          <b-icon-list color="#212529" font-scale="1.5" aria-hidden="true" variant="light" />
-        </div>
-      </b-nav-text>
-    </b-navbar-nav>
+  <b-navbar id="dash-nav" type="dark" variant="white" class="bg-transparent">
+    <b-container fluid>
+      <b-row no-gutters class="flex-grow-1">
+        <b-col cols="12" md="6" class="mb-3 mb-md-0">
+          <b-navbar-nav fill class="align-items-center">
+            <b-nav-text class="mr-3">
+              <div id="header-sidebar-toggle" class="cursor-pointer" @click="toggleSidebar">
+                <b-icon-list color="#212529" font-scale="1.5" aria-hidden="true" variant="light" />
+              </div>
+            </b-nav-text>
+
+            <b-nav-form class="flex-grow-1 mr-2 mr-md-0" form-class="flex-grow-1">
+              <b-form-input class="rounded-pill dashNav__search w-md-50" placeholder="Search Event or User" />
+            </b-nav-form>
+
+            <div class="d-md-none dashNav__tool rounded-pill">
+              <b-btn variant="link" :class="[...iconClass, 'px-2']" @click="collapseToolBox">
+                <b-icon v-bind="{ ...iconProps, fontScale: 0.9 }" icon="tools" />
+                <b-icon v-bind="{ ...iconProps, fontScale: 0.85 }" icon="chevron-down" />
+              </b-btn>
+            </div>
+          </b-navbar-nav>
+        </b-col>
+        <b-col cols="12" md="6" :class="['px-0 px-md-3 dashNav__toolWrapper', {active : toolboxCollapsed}]">
+          <b-navbar-nav align="end" class="align-items-center h-100">
+            <div class="d-flex flex-grow-1 flex-md-grow-0 justify-content-between rounded-pill py-2 py-md-0 dashNav__tool">
+              <b-btn variant="link" :class="iconClass">
+                <b-icon v-bind="iconProps" icon="arrow-clockwise" />
+              </b-btn>
+              <b-btn variant="link" :class="iconClass">
+                <b-icon v-bind="iconProps" icon="arrow-left-square" />
+              </b-btn>
+              <b-btn variant="link" :class="iconClass">
+                <b-icon v-bind="iconProps" icon="envelope-fill" />
+              </b-btn>
+              <b-btn variant="link" :class="iconClass">
+                <b-iconstack shift-h="-3" shift-v="3">
+                  <b-icon icon="bell-fill" scale="0.7" shift-v="0" />
+                  <b-icon icon="circle-fill" scale="0.2" shift-h="6" shift-v="5" variant="danger" />
+                </b-iconstack>
+              </b-btn>
+              <b-btn variant="link" :class="iconClass">
+                <b-icon v-bind="iconProps" icon="gear-wide-connected" />
+              </b-btn>
+            </div>
+          </b-navbar-nav>
+        </b-col>
+      </b-row>
+    </b-container>
   </b-navbar>
 </template>
 
 <script>
+import checkResolution from '~/mixins/checkResolution'
+
 export default {
   name: 'DashboardNav',
+  mixins: [
+    checkResolution
+  ],
+  data: () => ({
+    iconProps: {
+      fontScale: 1.5,
+      class: 'align-middle',
+      shiftV: 0.7
+    },
+    iconClass: ['text-decoration-none text-center'],
+    toolboxCollapsed: false
+  }),
+  computed: {},
+  mounted () {},
   methods: {
     toggleSidebar () {
       this.$emit('toggleSidebar')
+    },
+    collapseToolBox () {
+      this.toolboxCollapsed = this.isMobile
+        ? !this.toolboxCollapsed
+        : true
     }
   }
 }
