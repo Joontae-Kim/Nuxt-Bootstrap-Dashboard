a0e2ead5c2db8e8022b59c8f35fdca1583ee5171
김준태 <bexellenting@gmail.com>
2021-09-12 19:16:42 +0900

    Create layout of the authentication page

---
+++ layouts/authentication.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper vh-100">
+    <b-container fluid class="h-100 p-3 position-relative wrapper--authentication">
+      <template v-for="index in 6">
+        <span :key="index" :class="`circle circle-layer-${index}`" />
+      </template>
+      <Nuxt class="rounded" />
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthenticationLayout',
+  data: () => ({}),
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+@import '../assets/styles/layout/authentication.scss';
+</style>
