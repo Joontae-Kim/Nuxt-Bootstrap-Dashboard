57e0a4c0500eb0b894f0202e89abb691433a001e
김준태 <bexellenting@gmail.com>
2021-09-23 16:35:02 +0900

    Add authentication page link group

--- components/dashboard/sidebar.vue
+++ components/dashboard/sidebar.vue
@@ -39,13 +39,33 @@
               <b-nav-item class="ml-3 my-0" :to="{ name : 'usersList' }" exact-active-class="active"> List </b-nav-item>
             </b-nav>
           </b-collapse>
-          <b-nav-item exact-active-class="active">
+          <div :class="['d-flex justify-content-between align-items-center sidebar__parentlink', { active: $route.meta.parent === 'authentication' }]">
+            <b-nav-item :to="{ name: 'signin' }" :active="$route.meta.parent === 'authentication'">
+              Auth
+            </b-nav-item>
+            <b-btn variant="link" size="sm" class="sidebar__toggle p-0">
+              <b-icon :icon="authNestedToggled ? 'chevron-up' : 'chevron-down'" @click="authNestedToggled = !authNestedToggled" />
+            </b-btn>
+          </div>
+          <b-collapse id="nav-events-child" v-model="authNestedToggled">
+            <b-nav vertical>
+              <b-nav-item class="ml-3 my-0" :to="{ name: 'signin' }" exact-active-class="active"> Sign In </b-nav-item>
+              <b-nav-item class="ml-3 my-0" :to="{ name : 'signup' }" exact-active-class="active"> Sign Up </b-nav-item>
+              <b-nav-item class="ml-3 my-0" :to="{ name : 'resetpassword' }" exact-active-class="active"> Reset Password </b-nav-item>
+            </b-nav>
+          </b-collapse>
+          <!-- <b-nav-item exact-active-class="active">
             Another Link
-          </b-nav-item>
+          </b-nav-item> -->
         </b-nav>
       </b-navbar>
     </div>
     <div class="sidebar__footer">
+      <b-nav>
+        <b-nav-item :to="{ name: 'signin'}" link-classes="p-0">
+          Logout
+        </b-nav-item>
+      </b-nav>
       <div class="sidebar__close cursor-pointer ml-auto" @click="toggleSidebar">
         <template v-if="!collapsed">
           <b-icon icon="arrow-bar-right" font-scale="1.5" title="Open Sidebar" aria-hidden="true" shift-v="-1" />
@@ -73,8 +93,18 @@ export default {
   data: () => ({
     eventsNestedToggled: false,
     usersNestedToggled: false,
+    authNestedToggled: false,
     isPageMove: false
   }),
+  computed: {
+    pairOfNestedRoute () {
+      return {
+        users: 'usersNestedToggled',
+        events: 'eventsNestedToggled',
+        authentication: 'authNestedToggled'
+      }
+    }
+  },
   watch: {
     '$route.meta': {
       immediate: true,
@@ -82,18 +112,17 @@ export default {
       handler (val) {
         if (!!val.parent && ['users', 'events'].includes(val.parent)) {
           this.$nextTick(() => {
-            if (val.parent === 'users') {
-              this.usersNestedToggled = true
-              this.eventsNestedToggled = false
-            } else if (val.parent === 'events') {
-              this.eventsNestedToggled = true
-              this.usersNestedToggled = false
+            const activeNestedRoute = this.pairOfNestedRoute[val.parent]
+            if (activeNestedRoute) {
+              for (const [key, value] of Object.entries(this.pairOfNestedRoute)) {
+                if (val.parent !== key) { this[value] = false }
+              }
+              this[activeNestedRoute] = true
             }
           })
         } else {
           this.$nextTick(() => {
-            this.eventsNestedToggled = false
-            this.usersNestedToggled = false
+            Object.values(this.pairOfNestedRoute).forEach((nestedRouteStatus) => { this[nestedRouteStatus] = false })
           })
         }
       }
