4f2173a6ee2f0e60c408c30bf8fb3c6af5d8e7e7
김준태 <bexellenting@gmail.com>
2021-09-24 02:17:19 +0900

    Create a page of 500 error

--- pages/500.vue
+++ pages/500.vue
@@ -1,18 +1,29 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col cols>
-        <h2>Internal Server Error</h2>
-      </b-col>
-    </b-row>
-  </b-container>
+  <b-row class="h-100" align-v="center">
+    <b-col cols="12" class="d-block d-sm-none text-center mt-auto mb-5">
+      <b-icon icon="exclamation" font-scale="7" class="utility__icon" />
+    </b-col>
+    <b-col order="1" order-md="2" cols="12" md="6" class="d-none d-md-block text-center">
+      <b-icon icon="exclamation" font-scale="20" class="utility__icon" />
+    </b-col>
+    <b-col cols="12" md="6" order="2" order-md="1" class="text-white text-center text-md-center mt-sm-auto mt-md-0 mb-auto mb-md-0">
+      <h1 class="utility__title">500</h1>
+      <h2 class="utility__message mb-4 mb-md-5">Internal Server Error.</h2>
+      <div>
+        <b-btn variant="light" class="rounded-0 utility__link" :to="{ name: 'overview' }">Return to Dashboard</b-btn>
+      </div>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
 export default {
+  name: 'ServerError',
+  layout: 'utility',
+  data: () => ({})
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import '../assets/styles/layout/utility.scss';
 </style>
