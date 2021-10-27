6824119c40544a371f240f9c624cdb9295b550c2
김준태 <bexellenting@gmail.com>
2021-10-27 19:54:38 +0900

    Create the features-section's interaction

--- components/landing/features-section.vue
+++ components/landing/features-section.vue
@@ -1,7 +1,10 @@
 <template>
-  <b-row class="py-5 mb-5 bg-white landingFeature justify-content-center">
+  <b-row id="featureSection" class="py-5 mb-5 bg-white landingFeature justify-content-center">
     <b-col class="text-center mb-5" cols="12">
-      <h2>Featured By</h2>
+      <!-- <transition name="fade" mode="in-out">
+        <h2 v-show="isShowTitle" class="featureSection__title">Featured By</h2>
+      </transition> -->
+      <h2 class="featureSection__title">Featured By</h2>
     </b-col>
     <b-col
       cols="12"
@@ -12,39 +15,78 @@
       class="landingFeature__body"
     >
       <b-row>
-        <b-col cols v-bind="featureColumns" class="mb-4">
+        <b-col cols v-bind="featureColumns" class="mb-4 featureEle featureEle--1">
           <feature
             title="Nuxt.js"
             description="Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components."
           >
-            <img src="~/static/nuxt_svg.svg" title="Nuxt.js" alt="Nuxt.js" height="40" class="mb-3 mb-md-auto">
+            <!-- src="~/static/nuxt_svg.svg" -->
+            <img
+              src="~/static/nuxt_svg.svg"
+              title="Nuxt.js"
+              alt="Nuxt.js"
+              height="40"
+              class="mb-3 mb-md-auto"
+              data-src="/nuxt_svg.svg"
+              data-rd-md="container"
+              data-loaded="false"
+            >
           </feature>
         </b-col>
-        <b-col cols v-bind="featureColumns" class="mb-4">
+        <b-col cols v-bind="featureColumns" class="mb-4 featureEle featureEle--2">
           <feature
             title="BootstrapVue"
             description="Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components."
           >
-            <img src="~/static/bootstrapVue_svg.svg" title="BootstrapVue" alt="BootstrapVue" height="40" class="mb-3">
+            <!-- src="~/static/bootstrapVue_svg.svg" -->
+            <img
+              src="~/static/bootstrapVue_svg.svg"
+              title="BootstrapVue"
+              alt="BootstrapVue"
+              height="40"
+              class="mb-3"
+              data-src="/bootstrapVue_svg.svg"
+              data-rd-md="container"
+              data-loaded="false"
+            >
           </feature>
         </b-col>
-        <b-col cols v-bind="featureColumns" class="mb-4">
+        <b-col cols v-bind="featureColumns" class="mb-4 featureEle featureEle--3">
           <feature
             title="Express.js"
             description="Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components."
           >
-            <img src="~/static/expressjs.svg" title="Express.js" alt="Express.js" height="30" class="mb-3 mb-lg-auto">
+            <img
+              src="~/static/expressjs.svg"
+              title="Express.js"
+              alt="Express.js"
+              height="30"
+              class="mb-3 mb-lg-auto"
+              data-src="/expressjs.svg"
+              data-rd-md="container"
+              data-loaded="false"
+            >
           </feature>
         </b-col>
-        <b-col cols v-bind="featureColumns" class="mb-4 mb-lg-0">
+        <b-col cols v-bind="featureColumns" class="mb-4 mb-lg-0 featureEle featureEle--1">
           <feature
             title="Axios Module for Nuxt.js"
             description="Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components."
           >
-            <img src="~/static/AxiosModule_svg.svg" title="Axios Module for Nuxt.js" alt="Axios Module for Nuxt.js" height="20" class="mb-3 mb-lg-auto">
+            <!-- src="~/static/AxiosModule_svg.svg" -->
+            <img
+              src="~/static/AxiosModule_svg.svg"
+              title="Axios Module for Nuxt.js"
+              alt="Axios Module for Nuxt.js"
+              height="20"
+              class="mb-3 mb-lg-auto"
+              data-src="/AxiosModule_svg.svg"
+              data-rd-md="container"
+              data-loaded="false"
+            >
           </feature>
         </b-col>
-        <b-col cols v-bind="featureColumns" class="mb-4 mb-lg-0">
+        <b-col cols v-bind="featureColumns" class="mb-4 mb-lg-0 featureEle featureEle--2">
           <feature
             title="Layout with Sidebar and Nav"
             description="Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components."
@@ -52,7 +94,7 @@
             <BIconLayoutTextWindowReverse title="Layout with Sidebar and Nav" font-scale="2" variant="dark" class="mb-3" />
           </feature>
         </b-col>
-        <b-col cols v-bind="featureColumns" class="mb-sm-4 mb-lg-0">
+        <b-col cols v-bind="featureColumns" class="mb-sm-4 mb-lg-0 featureEle featureEle--3">
           <feature
             title="Responsive Design"
             description="Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components."
@@ -85,12 +127,40 @@ export default {
     featureColumnClass: {
       top: 'mb-md-4',
       bottom: ''
-    }
-  })
+    },
+    FSObserver: null,
+    isShowTitle: false
+  }),
+  created () {},
+  mounted () {
+    this.observeFeatureSection()
+  },
+  methods: {
+    observeFeatureSection () {
+      const fsection = document.getElementById('featureSection')
+      this.FSObserver = new IntersectionObserver(
+        this.onFTSectionObserved,
+        { rootMargin: '50px 0px', threshold: 0 }
+      )
+      this.FSObserver.observe(fsection)
+    },
+    onFTSectionObserved (entries, observer) {
+      entries.forEach(({ target, isIntersecting }) => {
+        if (isIntersecting) {
+          target.classList.add('active')
+        } else {
+          target.classList.remove('active')
+        }
+      })
+    },
+    observeFeatureEle () {}
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+$transition-time-featureSection: 0.5s;
+
 .landingFeature {
   position: relative;
   overflow: hidden;
@@ -99,52 +169,82 @@ export default {
     z-index: 1;
   }
 
-  &::before {
+  &::before,
+  &::after {
     position: absolute;
     content: " ";
+    padding: 0;
+    transition-property: padding;
+    transition-delay: $transition-time-featureSection;
+    transition-duration: $transition-time-featureSection;
+    transition-timing-function: ease-in-out;
+  }
+
+  &::before {
     background: linear-gradient(135deg, #153D77, #2F70AF);
     border-radius: 0 0 30rem 0;
     left: 0;
     box-shadow: 4px 0.5rem 0.25rem rgb(0 0 0 / 8%);
-
     @media (max-width: 575.98px) {
       top: 7rem;
-      padding: 10rem 17rem 17rem 6.5rem;
     }
 
-    @media (min-width: 576px) and (max-width: 991.98px) {
+    @media (min-width: 576px) {
       top: 0rem;
-      padding: 9.5rem;
-    }
-
-    @media (min-width: 992px) {
-      top: 0rem;
-      padding: 12rem;
     }
   }
 
   &::after {
-    position: absolute;
-    content: " ";
     background: linear-gradient(135deg, #2F70AF, #153D77);
     border-radius: 30rem 0 0 0;
     right: 0;
     box-shadow: -4px -0.5rem 0.25rem rgb(0 0 0 / 8%);
+    bottom: 0rem;
+  }
 
-    @media (max-width: 575.98px) {
-      bottom: 0rem;
-      padding: 10rem 13.5rem 10rem 10rem;
+  &.active {
+    &::before {
+      @media (max-width: 575.98px) {
+        padding: 10rem 17rem 17rem 6.5rem;
+      }
+
+      @media (min-width: 576px) and (max-width: 991.98px) {
+        padding: 9.5rem;
+      }
+
+      @media (min-width: 992px) {
+        padding: 12rem;
+      }
     }
 
-    @media (min-width: 576px) and (max-width: 991.98px) {
-      bottom: 0;
-      padding: 5rem;
-    }
+    &::after {
+      @media (max-width: 575.98px) {
+        padding: 10rem 13.5rem 10rem 10rem;
+      }
 
-    @media (min-width: 992px) {
-      bottom: 0;
-      padding: 6.5rem;
+      @media (min-width: 576px) and (max-width: 991.98px) {
+        padding: 5rem;
+      }
+
+      @media (min-width: 992px) {
+        padding: 6.5rem;
+      }
     }
   }
+}
+
+.featureEle {
+  transition-property: opacity,;
+  transition-delay: $transition-time-featureSection * 1.3;
+  transition-duration: $transition-time-featureSection + 0.5ms;
+  margin-bottom: 3.5rem;
+}
+
+.landingFeature:not(.active) .featureEle {
+  opacity: 0.3;
+}
+
+.landingFeature.active .featureEle {
+  opacity: 1;
 }
 </style>
