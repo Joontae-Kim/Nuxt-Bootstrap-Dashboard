5b05dd5f9a6ccf5dc1015b14360dd8c33fba5f17
김준태 <bexellenting@gmail.com>
2021-11-05 16:02:54 +0900

    Enhanced the Header section's interaction

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,38 @@
 <template>
   <fragment>
     <BaseHeader />
-    <div :class="[loading ? 'd-flex' : 'd-none', 'align-items-center justify-content-center vh-100 vw-100 bg-light overflow-hidden']">
-      Loading
-    </div>
-    <transition name="fade" mode="out-in">
-      <main v-show="!loading">
-        <b-row id="target" class="mx-0 landingIntro">
-          <b-col cols="12" md="7" lg="6" class="text-white align-self-center pt-4 pb-5 py-sm-0">
-            <h1 class="">It's Nuxt Dashboard Propject</h1>
-            <h3 class="font-weight-light mb-1">
-              with Bootstrap and BootstrapVue
-            </h3>
-          </b-col>
+    <main>
+      <b-row id="header" :class="['mx-0 landingIntro', { loaded }]">
+        <b-col
+          cols="12"
+          v-bind="loaded ? {md: 7, lg: 6} : {md: 12}"
+          :class="['align-self-center text-white pt-4 pb-5 py-sm-0 landingIntro__content', { loaded }]"
+        >
+          <h1 class="">It's Nuxt Dashboard Propject</h1>
+          <h3 class="font-weight-light mb-1">
+            with Bootstrap and BootstrapVue
+          </h3>
+        </b-col>
+        <template v-if="imgloaded.status">
           <b-col
             cols="12"
-            md="5"
-            lg="6"
-            class="h-50 h-md-100"
+            v-bind="imgloaded.status ? {md: 5, lg: 6} : {md: 12}"
+            :class="['landingIntro__img', imgloaded.class]"
           >
             <img
               id="header-snapshot"
-              data-src="/overview-page.png"
-              data-active-container="#target"
-              data-loaded="false"
+              src="/overview-page.png"
+              data-active-container="#header"
+              :data-loaded="imgloaded.status"
               data-rd-md="container"
               alt="overview page"
               class="shadow landingIntro__screenshot"
             >
           </b-col>
-        </b-row>
-        <Nuxt />
-      </main>
-    </transition>
-    <!-- footer -->
+        </template>
+      </b-row>
+      <Nuxt />
+    </main>
     <BaseFooter />
   </fragment>
 </template>
@@ -46,15 +45,30 @@ export default {
     Fragment
   },
   data: () => ({
-    loading: true,
+    loaded: false,
+    imgloaded: {
+      status: false,
+      class: null
+    },
     headerObserver: null
   }),
   created () {},
   mounted () {
-    this.observeHeroImgHandler()
+    document.body.classList.add('overflow-hidden')
     setTimeout(() => {
-      this.loading = false
-    }, 1000)
+      this.loaded = true
+    }, 1500)
+    setTimeout(() => {
+      this.imgloaded.status = true
+      this.imgloaded.class = 'loading'
+    }, 1700)
+    setTimeout(() => {
+      this.imgloaded.class = 'loaded'
+      this.$nextTick(() => {
+        this.observeHeroImgHandler()
+        document.body.classList.remove('overflow-hidden')
+      })
+    }, 1800)
   },
   beforeDestroy () {
     this.headerObserver.disconnect()
@@ -70,24 +84,10 @@ export default {
     },
     onHeaderElementObserved (entries) {
       entries.forEach(({ target, isIntersecting }) => {
-        if (target.dataset.src && isIntersecting) {
-          target.src = target.dataset.src
-          target.dataset.src = ''
-          if (target.dataset.loaded === 'false') {
-            target.dataset.loaded = true
-            if (target.dataset.activeContainer) {
-              const activeContainer = document.querySelector(target.dataset.activeContainer)
-              activeContainer.classList.add('active')
-            }
-          }
-        }
-        if (target.dataset.loaded === 'true' && !!target.dataset.activeContainer) {
-          const activeContainer = document.querySelector(target.dataset.activeContainer)
-          if (isIntersecting) {
-            activeContainer.classList.add('active')
-          } else {
-            activeContainer.classList.remove('active')
-          }
+        if (isIntersecting) {
+          target.classList.remove('unseen')
+        } else {
+          target.classList.add('unseen')
         }
       })
     }
@@ -101,50 +101,85 @@ export default {
   background: linear-gradient(45deg, #002966, #153D77, #2F70AF);
   margin-bottom: 4rem;
   overflow: hidden;
-  transition: padding .25s;
+  padding: 2rem 0rem;
+  height: 100%;
+  transition: min-height 1.5s ease-out;
 
-  @media (max-width: 1199.98px) {
-    padding: 2rem 0rem;
+  &.loaded {
+    min-height: 1vh;
   }
 
-  @media (min-width: 1200px) {
-    padding: 0;
+  &:not(.loaded) {
+    min-height: 100vh;
+  }
 
-    &.active {
-      padding: 2rem 0rem;
+  &__content {
+    transition: text-align 0.3s ease-in,
+      max-width  0.5s ease-in-out;
+
+    &:not(.loaded) {
+      text-align: center;
+    }
+
+    &.loaded {
+      text-align: left;
+    }
+  }
+
+  &__img {
+    opacity: 0;
+    max-height: 0vh;
+    transition: max-width, max-height 0.5s ease-in-out .5s,
+      opacity 1s ease-in-out;
+
+    &.loading {
+      max-height: 100vh;
+      @media (max-width: 639.8px) {
+        height: 50%;
+      }
+
+      @media (min-width: 640px) {
+        height: 100%;
+      }
+    }
+
+    &.loaded {
+      opacity: 1;
+      max-height: 100vh;
+      @media (max-width: 639.8px) {
+        height: 50%;
+      }
+
+      @media (min-width: 640px) {
+        height: 100%;
+      }
     }
   }
 
   &__screenshot {
     width: 100%;
     border-radius: 3px;
+    transition: transform .5s ease-in-out;
 
     &[data-loaded="true"] {
-      transition: transform .5s;
-    }
-  }
-
-  &.active .landingIntro__screenshot[data-loaded="true"] {
-    @media (max-width: 575.98px) {
-      transform: translateY(-0.5rem);
-    }
-
-    @media (min-width: 1200px) {
-      transform: translateY(4rem);
-
-      &:hover {
-        transform: translateY(0.5rem) !important;
+      box-shadow: 0 0.5rem 1rem rgb(0 0 0 / 15%);
+      @media (max-width: 575.98px) {
+        transform: translateY(-0.5rem);
       }
-    }
-  }
 
-  &:not(.active) .landingIntro__screenshot[data-loaded="true"] {
-    @media (max-width: 767.98px) {
-      transform: translateY(3.5rem);
-    }
+      @media (min-width: 1200px) {
+        transform: translateY(0rem);
+      }
 
-    @media (min-width: 768px) {
-      transform: translateY(4rem);
+      &.unseen {
+        @media (max-width: 767.98px) {
+          transform: translateY(3.5rem);
+        }
+
+        @media (min-width: 768px) {
+          transform: translateY(4rem);
+        }
+      }
     }
   }
 }
