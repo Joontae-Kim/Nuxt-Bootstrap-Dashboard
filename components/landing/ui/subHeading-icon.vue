df1b12e8a54c72108fe4da0992498a59c10a1fff
김준태 <bexellenting@gmail.com>
2021-11-18 02:03:12 +0900

    Enhanced a feature of the subHeading section

---
+++ components/landing/ui/subHeading-icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <img :data-src="icon" :alt="title" :title="title" style="width: 2rem;">
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      required: true,
+      type: String
+    },
+    title: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
