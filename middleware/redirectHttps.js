0d1f629057ededb8d588c9a8955a2da8a89bf200
김준태 <bexellenting@gmail.com>
2021-12-17 19:56:34 +0900

    Fixed the bug of generating the encrypted url

--- middleware/redirectHttps.js
+++ middleware/redirectHttps.js
@@ -2,13 +2,17 @@ export default function ({ isDev, req, redirect }) {
   const requestURL = process.server
     ? new URL(req.headers.referer)
     : new URL(window.location.origin)
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`requestURL => ${requestURL}`)
+  }
   const isEncrypted = requestURL.protocol === 'https:'
   const requestedFullURL = process.server
     ? requestURL.href
     : window.location.href
-  const redirectHTTPSURL = requestedFullURL.replace('http', 'https')
+  const redirectHTTPSURL = requestedFullURL.replace('http:', 'https:')
   if (process.env.NODE_ENV !== 'production') {
-    console.log('redirectHTTPSURL => ', redirectHTTPSURL)
+    console.log(`requestedFullURL => ${requestedFullURL}`)
+    console.log(`redirectHTTPSURL => ${redirectHTTPSURL}`)
   }
   if (process.env.NODE_ENV !== 'development' && !isEncrypted) {
     redirect(redirectHTTPSURL)
