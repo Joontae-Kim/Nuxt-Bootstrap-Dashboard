f40ccc824986e0af37b58669457d710c76e21792
김준태 <bexellenting@gmail.com>
2021-12-17 20:23:10 +0900

    Test redirectHTTPs on staging

--- middleware/redirectHttps.js
+++ middleware/redirectHttps.js
@@ -1,20 +1,22 @@
-export default function ({ isDev, req, redirect }) {
+export default function ({ req, redirect }) {
   const requestURL = process.server
     ? new URL(req.headers.referer)
     : new URL(window.location.origin)
+  const isEncrypted = requestURL.protocol === 'https:'
   if (process.env.NODE_ENV !== 'production') {
     console.log(`requestURL => ${requestURL}`)
+    console.log(`isEncrypted => ${isEncrypted}`)
+    console.log(`requestURL.protocol => ${requestURL.protocol}`)
   }
-  const isEncrypted = requestURL.protocol === 'https:'
   const requestedFullURL = process.server
     ? requestURL.href
     : window.location.href
-  const redirectHTTPSURL = requestedFullURL.replace('http:', 'https:')
   if (process.env.NODE_ENV !== 'production') {
     console.log(`requestedFullURL => ${requestedFullURL}`)
-    console.log(`redirectHTTPSURL => ${redirectHTTPSURL}`)
   }
   if (process.env.NODE_ENV !== 'development' && !isEncrypted) {
+    const redirectHTTPSURL = requestedFullURL.replace('http:', 'https:')
+    console.log(`redirectHTTPSURL => ${redirectHTTPSURL}`)
     redirect(redirectHTTPSURL)
   }
 }
