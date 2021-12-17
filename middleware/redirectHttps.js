e0c5060ab336a7b99ebb18e8c87ab4245c6a3b28
김준태 <bexellenting@gmail.com>
2021-12-18 02:12:17 +0900

    Fixed the bug of redirectHttps

--- middleware/redirectHttps.js
+++ middleware/redirectHttps.js
@@ -1,9 +1,6 @@
 export default function ({ req, redirect }) {
   let isEncrypted = true
   if (process.server) {
-    // x-forwarded-proto
-    console.log(`req.headers => `, req.headers)
-    console.log(`req.originalUrl => `, req.originalUrl)
     isEncrypted = req.headers['x-forwarded-proto'] === 'https'
   }
 
@@ -11,12 +8,10 @@ export default function ({ req, redirect }) {
     isEncrypted = new URL(window.location.origin).protocol === 'https:'
   }
 
-  console.log('isEncrypted: ', isEncrypted)
   if (process.env.NODE_ENV !== 'development' && !isEncrypted) {
     const redirectHTTPSURL = process.server
       ? 'https://' + req.headers.host + req.originalUrl
       : window.location.href.replace('http', 'https')
-    console.log(`redirectHTTPSURL => ${redirectHTTPSURL}`)
-    // redirect(redirectHTTPSURL)
+    redirect(redirectHTTPSURL)
   }
 }
