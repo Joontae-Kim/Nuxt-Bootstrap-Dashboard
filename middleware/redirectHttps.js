c50655e6a10b622261462e90b055d758a73e95a9
김준태 <bexellenting@gmail.com>
2021-12-17 20:47:11 +0900

    Check request url

--- middleware/redirectHttps.js
+++ middleware/redirectHttps.js
@@ -1,13 +1,21 @@
 export default function ({ req, redirect }) {
-  const requestURL = process.server
-    ? new URL(req.headers.referer)
-    : new URL(window.location.origin)
-  const isEncrypted = requestURL.protocol === 'https:'
-  if (process.env.NODE_ENV !== 'production') {
-    console.log(`requestURL => ${requestURL}`)
-    console.log(`isEncrypted => ${isEncrypted}`)
-    console.log(`requestURL.protocol => ${requestURL.protocol}`)
+  if (process.server) {
+    console.log(`req.headers.host => `, req.headers.host)
+    console.log(`req.headers.referer => `, req.headers.referer)
   }
+
+  if (process.client) {
+    console.log(`window.location.origin => `, window.location.origin)
+  }
+  // const requestURL = process.server
+  //   ? new URL(req.headers.referer)
+  //   : new URL(window.location.origin)
+  // const isEncrypted = requestURL.protocol === 'https:'
+  // if (process.env.NODE_ENV !== 'production') {
+  //   console.log(`requestURL => ${requestURL}`)
+  //   console.log(`isEncrypted => ${isEncrypted}`)
+  //   console.log(`requestURL.protocol => ${requestURL.protocol}`)
+  // }
   // const requestedFullURL = process.server
   //   ? requestURL.href
   //   : window.location.href
